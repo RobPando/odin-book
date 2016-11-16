5e31b77240aca13dbeb46e555c428169c4964a67
Roberto Pando <roberto_pando@hotmail.com>
2016-11-16 21:59:36 +0100

    comments model created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
   validates :first_name, presence: true
   validates :last_name, presence: true
+
   has_many :posts
+  has_many :comments
 
   def full_name
     first_name + ' ' + last_name
