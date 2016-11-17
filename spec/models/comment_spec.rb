df2b0d23ec2ac00d16e754b2684abdacf7a40448
Roberto Pando <roberto_pando@hotmail.com>
2016-11-17 11:55:40 +0100

    final basic test on Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment do
-  it 'reply cannot be blank' do
-    @post = create(:post, user_id: 1)
-    @comment = build(:comment, user_id: 1, post_id: 1, reply: ' ')
-    @comment.valid?
-    expect(@comment.errors[:reply]).to include("can't be blank")
+
+  context "Creating a comment" do
+    before :each do
+      @post = create(:post, user_id: 1)
+      @comment = build(:comment, user_id: 1, post_id: 1)
+    end
+    it 'reply cannot be blank' do
+      @post = create(:post, user_id: 1)
+      @comment.reply = ' '
+      @comment.valid?
+      expect(@comment.errors[:reply]).to include("can't be blank")
+    end
+
+    it "comments on a post" do
+      @comment.reply = 'my reply'
+      @comment.save
+      expect(@comment).to be_valid
+   end
   end
 end
