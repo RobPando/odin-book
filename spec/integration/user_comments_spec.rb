20db8cda19e4c05616fe1dbea4a4c5e6d6d0e901
Roberto Pando <roberto_pando@hotmail.com>
2016-11-18 12:58:27 +0100

    post form

---
+++ spec/integration/user_comments_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'User comments' do
+end
+
+feature 'User comments on post', type: :feature do
+end
---
+++ spec/integration/user_posts_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'User posts' do
+end
+
+feature 'User writes post', type: :feature do
+end
