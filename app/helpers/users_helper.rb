c99dd8327baadcfa609b7ee67341a456bdfef8a9
Roberto Pando <roberto_pando@hotmail.com>
2016-11-20 22:54:59 +0100

    all friendship and requests UX complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,17 @@
 module UsersHelper
+  def established_friendship(other_user)
+    if current_user.friends.include?(other_user)
+      find_friendship(other_user.id)
+    else
+      find_inversefriendship(other_user.id)
+    end
+  end
 
+  def find_friendship(other_user_id)
+    current_user.friendships.find_by(friend_id: other_user_id)
+  end
+
+  def find_inversefriendship(other_user_id)
+    current_user.inverse_friendships.find_by(user_id: other_user_id)
+  end
 end
