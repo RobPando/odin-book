777b66f1d9b43a23dc5c1b36950155c32de6e150
Roberto Pando <roberto_pando@hotmail.com>
2016-11-21 21:18:29 +0100

    omniauth with facebook implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -81,4 +81,14 @@ class User < ApplicationRecord
   def format_last_name
     self.last_name = last_name.downcase.capitalize
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.avatar = auth.info.image
+    end
+  end
 end
