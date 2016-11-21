83c44a6cb4c29377d40a2e338534c4e77ee97566
Roberto Pando <roberto_pando@hotmail.com>
2016-11-21 21:20:56 +0100

    routes callback set

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  devise_for :users, controllers: { registrations: 'registrations' }
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { registrations: 'registrations',
+                                    omniauth_callbacks: "users/omniauth_callbacks" }
   resources :users, except: [:new, :create] do
     get '/profile', to: 'profiles#show'
     get '/profile/edit', to: 'profiles#edit'
