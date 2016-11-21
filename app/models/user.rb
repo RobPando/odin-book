c7024ed317deed5b0ed04ecc5f85d54334152ef5
Roberto Pando <roberto_pando@hotmail.com>
2016-11-21 22:32:48 +0100

    user model omniauth change

--- app/models/user.rb
+++ app/models/user.rb
@@ -100,7 +100,6 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.avatar = auth.info.image
